e10fa8d98078c7f2a0f3881f100cb218f540795a
imysh578 <imysh578@gmail.com>
2022-01-12 09:29:18 +0900

    add config.ts

---
+++ client/server/config.ts
@@ -0,0 +1,7 @@
+export const INITIAL_DIFFICULTY: number = 3;
+
+export const BLOCK_GENERATION_INTERVAL: number = 10; // number of blocks
+export const DIFFICULTY_ADJUSTMENT_INTERVAL: number = 10; // seconds
+export const MINE_INTERVAL: number =
+	BLOCK_GENERATION_INTERVAL * DIFFICULTY_ADJUSTMENT_INTERVAL; // seconds
+
