c7c0e4ba53a501fa9a517ea163e8c0710f3648e8
mnnm <alsrhld101@naver.com>
2022-01-13 14:02:27 +0900

    add main

--- client/server/main/main.ts
+++ client/server/main/main.ts
@@ -1,28 +1,44 @@
-import bodyParser from "body-parser";
-import express, { Request, Response } from "express";
-import "dotenv/config";
-import Blockchain from "../blockchain/blockchain";
+import bodyParser from 'body-parser';
+import express, { Request, Response } from 'express';
+import 'dotenv/config';
+import Blockchain from '../blockchain/blockchain';
+
+const blockchain: Blockchain = new Blockchain();
 
 const app = express();
-
 export const http_port: number =
-    parseInt(process.env.HTTP_PORT as string) || 3001;
+  parseInt(process.env.HTTP_PORT as string) || 3001;
 
 const initHttpServer = (port: number) => {
-    app.use(bodyParser.json());
+  app.use(bodyParser.json());
 
-    app.get("/", (req: Request, res: Response) => {
-        res.send("welcome!");
-    });
+  app.get('/', (req: Request, res: Response) => {
+    res.send('welcome!');
+  });
 
-    app.get("/blocks", (req: Request, res: Response) => {
-        const bc = new Blockchain().chain;
-        res.send(bc);
-    });
+  app.get('/balance', (req, res) => {});
+
+  app.get('/blocks', (request, response) => {
+    response.json(blockchain.chain);
+  });
+
+  app.get('/transactions', (req, res) => {
+    res.send('');
+  });
+
+  app.get('/public-key', (request, response) => {});
+
+  app.post('/mineBlock', (req: Request, res: Response) => {
+    const data: any = req.body.data;
+    console.log('mineBlock', data);
+    blockchain.addBlock(data);
+    console.log(blockchain);
+    res.send('ok');
+  });
 };
 
 const server = app.listen(http_port, () => {
-    console.log(`
+  console.log(`
     ####################################
     🛡️  Server listening on port: 3001🛡️
     ####################################`);
