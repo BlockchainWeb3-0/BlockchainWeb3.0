c68b67018a4a5187b1e973935c6db6df116b5896
imysh578 <imysh578@gmail.com>
2022-01-13 16:50:45 +0900

    create Nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,32 @@
-import './App.css';
+import "./App.css";
+import TopNav from "./components/navbar/TopNav";
+import SignIn from "./components/signin/SignIn";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import MainPage from "./components/mainPage/MainPage";
 
 function App() {
+	return (
+		<>
+			<TopNav/>
+      <RouterComp/>
+		</>
+	);
+}
+
+function RouterComp () {
   return (
-    <div className="App">
-    </div>
-  );
+    <>
+      <BrowserRouter>
+				<Routes>
+					<Route path="/" >
+						<Route index element={<MainPage />} />
+						<Route path="sign" element={<SignIn />}>
+						</Route>
+					</Route>
+				</Routes>
+			</BrowserRouter>
+    </>
+  )
 }
 
 export default App;
