cb28b746e19385690ec2a6e5686e41b6c8bbe864
mnnm <alsrhld101@naver.com>
2022-01-13 17:46:56 +0900

    khh_wallet

--- client/server/main/main.ts
+++ client/server/main/main.ts
@@ -1,73 +1,82 @@
-import bodyParser from "body-parser";
-import express, { Request, Response } from "express";
-import "dotenv/config";
-import Blockchain from "../blockchain/blockchain";
-import { initP2PServer, getSockets, connectToPeers } from "../p2p/p2p";
-import Block from "../blockchain/block";
+import bodyParser from 'body-parser';
+import express, { Request, Response } from 'express';
+import 'dotenv/config';
+import Blockchain from '../blockchain/blockchain';
+import { UnspentTxOut } from '../transaction/transaction';
+import { getTransactionPool } from '../transactionPool/transactionPool';
+import { getPublicFromWallet, initWallet } from '../wallet/wallet';
+import { initP2PServer, getSockets, connectToPeers } from '../p2p/p2p';
+import Block from '../blockchain/block';
 
 const blockchain: Blockchain = new Blockchain();
 
 const app = express();
 
 export const http_port: number =
-    parseInt(process.env.HTTP_PORT as string) || 3001;
+  parseInt(process.env.HTTP_PORT as string) || 3001;
 
 ////////////////////////////////////////
 // 최현석 P2P Test
 export const p2p_port: number =
-    parseInt(process.env.P2P_PORT as string) || 6001;
+  parseInt(process.env.P2P_PORT as string) || 6001;
 ////////////////////////////////////////
 
 const initHttpServer = (port: number) => {
-    app.use(bodyParser.json());
+  app.use(bodyParser.json());
 
-    app.get("/", (req: Request, res: Response) => {
-        res.send("welcome!");
+  app.get('/', (req: Request, res: Response) => {
+    res.send('welcome!');
+  });
+
+  app.get('/balance', (req: Request, res: Response) => {
+    const balance: number = 100;
+    res.send({ balance: balance });
+  });
+
+  app.get('/blocks', (req: Request, res: Response) => {
+    res.json(blockchain.chain);
+  });
+
+  app.get('/address', (req: Request, res: Response) => {
+    const address: string = getPublicFromWallet();
+    res.send({ address: address });
+  });
+
+  ////////////////////////////////////////
+  // 최현석 P2P Test
+  app.get('/peers', (req, res) => {
+    let sockInfo: string[] = [];
+    getSockets().forEach((s: any) => {
+      sockInfo.push(s._socket.remoteAddress + ':' + s._socket.remotePort);
+      console.log(s._socket);
     });
+    res.send(sockInfo);
+    //res.send(getSockets());
+  });
 
-    app.get("/balance", (req, res) => {});
+  app.post('/mineBlock', (req: Request, res: Response) => {
+    const data: any = req.body.data;
+    console.log('mineBlock', data);
+    blockchain.addBlock(data);
+    console.log(blockchain);
+    res.send('ok');
+  });
 
-    app.get("/blocks", (request, response) => {
-        response.json(blockchain.chain);
-    });
+  app.get('/transactionPool', (req, res) => {
+    res.send(getTransactionPool());
+  });
 
-    app.get("/transactions", (req, res) => {
-        res.send("");
-    });
-
-    app.get("/public-key", (request, response) => {});
-
-    ////////////////////////////////////////
-    // 최현석 P2P Test
-    app.get("/peers", (req, res) => {
-        let sockInfo: string[] = [];
-        getSockets().forEach((s: any) => {
-            sockInfo.push(s._socket.remoteAddress + ":" + s._socket.remotePort);
-            console.log(s._socket);
-        });
-        res.send(sockInfo);
-        //res.send(getSockets());
-    });
-
-    app.post("/mineBlock", (req: Request, res: Response) => {
-        const data: any = req.body.data;
-        console.log("mineBlock", data);
-        blockchain.addBlock(data);
-        console.log(blockchain);
-        res.send("ok");
-    });
-
-    app.post("/addPeer", (req: Request, res: Response) => {
-        const data: string[] = req.body.data;
-        connectToPeers(data);
-        console.log(data);
-        res.send(data);
-    });
-    ////////////////////////////////////////
+  app.post('/addPeer', (req: Request, res: Response) => {
+    const data: string[] = req.body.data;
+    connectToPeers(data);
+    console.log(data);
+    res.send(data);
+  });
+  ////////////////////////////////////////
 };
 
 const server = app.listen(http_port, () => {
-    console.log(`
+  console.log(`
     ####################################
     🛡️  Server listening on port: ${http_port}🛡️
     ####################################`);
--- client/server/wallet/wallet.ts
+++ client/server/wallet/wallet.ts
@@ -1,47 +1,187 @@
-import {ec} from "elliptic";
-import fs from "fs";
-import * as _ from "lodash";
+import { ec } from 'elliptic';
+import { existsSync, readFileSync, unlinkSync, writeFileSync } from 'fs';
+import _ from 'lodash';
+import {
+  getPublicKey,
+  getTransactionId,
+  signTxIn,
+  Transaction,
+  TxIn,
+  TxOut,
+  UnspentTxOut,
+} from '../transaction/transaction';
 
 const EC = new ec('secp256k1');
-const privateKeyLocation = process.env.PRIVATE_KEY || "node/wallet/private_key";
+const privateKeyLocation = process.env.PRIVATE_KEY || 'node/wallet/private_key';
 
-export default class Wallet {
-  static getPrivateFromWallet = (): string => {
-    const buffer = fs.readFileSync(privateKeyLocation, "utf8");
-    return buffer.toString();
+const getPrivateFromWallet = (): string => {
+  const buffer = readFileSync(privateKeyLocation, 'utf8');
+  return buffer.toString();
+};
+
+const getPublicFromWallet = (): string => {
+  const privateKey = getPrivateFromWallet();
+  const key = EC.keyFromPrivate(privateKey, 'hex');
+  return key.getPublic().encode('hex', false);
+};
+
+const generatePrivateKey = (): string => {
+  const keyPair = EC.genKeyPair();
+  const privateKey = keyPair.getPrivate();
+  return privateKey.toString(16);
+};
+
+const initWallet = () => {
+  // let's not override existing private keys
+  if (existsSync(privateKeyLocation)) {
+    return;
   }
+  const newPrivateKey = generatePrivateKey();
 
-  static getPulicFromWallet = (): string => {
-    const privateKey = this.getPrivateFromWallet();
-    const key = EC.keyFromPrivate(privateKey, "hex");
-    return key.getPublic().encode("hex", false)
+  writeFileSync(privateKeyLocation, newPrivateKey);
+  console.log(
+    'new wallet with private key created to : %s',
+    privateKeyLocation
+  );
+};
+
+const deleteWallet = () => {
+  if (existsSync(privateKeyLocation)) {
+    unlinkSync(privateKeyLocation);
   }
+};
 
-  static generatePrivateKey = (): string => {
-    const keyPair = EC.genKeyPair();
-    const privateKey = keyPair.getPrivate();
-    return privateKey.toString(16);
-  }
+const getBalance = (address: string, unspentTxOuts: UnspentTxOut[]): number => {
+  return _(findUnspentTxOuts(address, unspentTxOuts))
+    .map((uTxO: UnspentTxOut) => uTxO.amount)
+    .sum();
+};
 
-  static initWallet = () => {
-    if(fs.existsSync(privateKeyLocation)) {
-      return;
+const findUnspentTxOuts = (
+  ownerAddress: string,
+  unspentTxOuts: UnspentTxOut[]
+) => {
+  return _.filter(
+    unspentTxOuts,
+    (uTxO: UnspentTxOut) => uTxO.address === ownerAddress
+  );
+};
+
+const findTxOutsForAmount = (
+  amount: number,
+  myUnspentTxOuts: UnspentTxOut[]
+) => {
+  let currentAmount = 0;
+  const includedUnspentTxOuts = [];
+  for (const myUnspentTxOut of myUnspentTxOuts) {
+    includedUnspentTxOuts.push(myUnspentTxOut);
+    currentAmount = currentAmount + myUnspentTxOut.amount;
+    if (currentAmount >= amount) {
+      const leftOverAmount = currentAmount - amount;
+      return { includedUnspentTxOuts, leftOverAmount };
     }
-    const newPrivateKey = this.generatePrivateKey();
-
-    fs.writeFileSync(privateKeyLocation, newPrivateKey);
-    console.log("new wallet with private key created to : %s", privateKeyLocation);
   }
 
-  static deleteWallet = () => {
-    if(fs.existsSync(privateKeyLocation)) {
-      fs.unlinkSync(privateKeyLocation);
+  const eMsg =
+    'Cannot create transaction from the available unspent transaction outputs.' +
+    ' Required amount:' +
+    amount +
+    '. Available unspentTxOuts:' +
+    JSON.stringify(myUnspentTxOuts);
+  throw Error(eMsg);
+};
+
+const createTxOuts = (
+  receiverAddress: string,
+  myAddress: string,
+  amount: any,
+  leftOverAmount: number
+) => {
+  const txOut1: TxOut = new TxOut(receiverAddress, amount);
+  if (leftOverAmount === 0) {
+    return [txOut1];
+  } else {
+    const leftOverTx = new TxOut(myAddress, leftOverAmount);
+    return [txOut1, leftOverTx];
+  }
+};
+
+const filterTxPoolTxs = (
+  unspentTxOuts: UnspentTxOut[],
+  transactionPool: Transaction[]
+): UnspentTxOut[] => {
+  const txIns: TxIn[] = _(transactionPool)
+    .map((tx: Transaction) => tx.txIns)
+    .flatten()
+    .value();
+  const removable: UnspentTxOut[] = [];
+  for (const unspentTxOut of unspentTxOuts) {
+    const txIn = _.find(txIns, (aTxIn: TxIn) => {
+      return (
+        aTxIn.txOutIndex === unspentTxOut.txOutIndex &&
+        aTxIn.txOutId === unspentTxOut.txOutId
+      );
+    });
+
+    if (txIn === undefined) {
+    } else {
+      removable.push(unspentTxOut);
     }
   }
 
-  // static getBalance = (address: string, UTXOs: UTXO[]): number => {
-  //   return _(findUTXOs(address, UTXOs)).map((UTXO: UTXO)=>UTXO.amount).sum();
-  // }
-}
+  return _.without(unspentTxOuts, ...removable);
+};
 
+const createTransaction = (
+  receiverAddress: string,
+  amount: number,
+  privateKey: string,
+  unspentTxOuts: UnspentTxOut[],
+  txPool: Transaction[]
+): Transaction => {
+  console.log('txPool: %s', JSON.stringify(txPool));
+  const myAddress: string = getPublicKey(privateKey);
+  const myUnspentTxOutsA = unspentTxOuts.filter(
+    (uTxO: UnspentTxOut) => uTxO.address === myAddress
+  );
 
+  const myUnspentTxOuts = filterTxPoolTxs(myUnspentTxOutsA, txPool);
+
+  // filter from unspentOutputs such inputs that are referenced in pool
+  const { includedUnspentTxOuts, leftOverAmount } = findTxOutsForAmount(
+    amount,
+    myUnspentTxOuts
+  );
+
+  const toUnsignedTxIn = (unspentTxOut: UnspentTxOut) => {
+    const txIn: TxIn = new TxIn();
+    txIn.txOutId = unspentTxOut.txOutId;
+    txIn.txOutIndex = unspentTxOut.txOutIndex;
+    return txIn;
+  };
+
+  const unsignedTxIns: TxIn[] = includedUnspentTxOuts.map(toUnsignedTxIn);
+
+  const tx: Transaction = new Transaction();
+  tx.txIns = unsignedTxIns;
+  tx.txOuts = createTxOuts(receiverAddress, myAddress, amount, leftOverAmount);
+  tx.id = getTransactionId(tx);
+
+  tx.txIns = tx.txIns.map((txIn: TxIn, index: number) => {
+    txIn.signature = signTxIn(tx, index, privateKey, unspentTxOuts);
+    return txIn;
+  });
+
+  return tx;
+};
+
+export {
+  createTransaction,
+  getPublicFromWallet,
+  getPrivateFromWallet,
+  getBalance,
+  generatePrivateKey,
+  initWallet,
+  deleteWallet,
+  findUnspentTxOuts,
+};
