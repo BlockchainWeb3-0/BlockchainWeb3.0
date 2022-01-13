02259bb2911d9bbba69c5bcd910f2d368aca1f96
Tozinoo <goldboy122@gmail.com>
2022-01-13 14:26:08 +0900

    add wallet

--- client/server/wallet/wallet.ts
+++ client/server/wallet/wallet.ts
@@ -1,47 +1,48 @@
-import {ec} from "elliptic";
+import { ec } from "elliptic";
 import fs from "fs";
 import * as _ from "lodash";
 
-const EC = new ec('secp256k1');
-const privateKeyLocation = process.env.PRIVATE_KEY || "node/wallet/private_key";
+const EC: ec = new ec("secp256k1");
+const privateKeyLocation: string =
+    process.env.PRIVATE_KEY || "node/wallet/private_key";
 
 export default class Wallet {
-  static getPrivateFromWallet = (): string => {
-    const buffer = fs.readFileSync(privateKeyLocation, "utf8");
-    return buffer.toString();
-  }
+    static generatePrivateKey = (): string => {
+        const keyPair = EC.genKeyPair();
+        const privateKey = keyPair.getPrivate();
+        return privateKey.toString(16);
+    };
 
-  static getPulicFromWallet = (): string => {
-    const privateKey = this.getPrivateFromWallet();
-    const key = EC.keyFromPrivate(privateKey, "hex");
-    return key.getPublic().encode("hex", false)
-  }
+    static initWallet = () => {
+        if (fs.existsSync(privateKeyLocation)) {
+            return;
+        }
+        const newPrivateKey = this.generatePrivateKey();
 
-  static generatePrivateKey = (): string => {
-    const keyPair = EC.genKeyPair();
-    const privateKey = keyPair.getPrivate();
-    return privateKey.toString(16);
-  }
+        fs.writeFileSync(privateKeyLocation, newPrivateKey);
+        console.log(
+            "new wallet with private key created to : %s",
+            privateKeyLocation
+        );
+    };
+    static getPrivateFromWallet = (): string => {
+        const buffer = fs.readFileSync(privateKeyLocation, "utf8");
+        return buffer.toString();
+    };
 
-  static initWallet = () => {
-    if(fs.existsSync(privateKeyLocation)) {
-      return;
-    }
-    const newPrivateKey = this.generatePrivateKey();
+    static getPulicFromWallet = (): string => {
+        const privateKey = this.getPrivateFromWallet();
+        const key = EC.keyFromPrivate(privateKey, "hex");
+        return key.getPublic().encode("hex", false);
+    };
 
-    fs.writeFileSync(privateKeyLocation, newPrivateKey);
-    console.log("new wallet with private key created to : %s", privateKeyLocation);
-  }
+    static deleteWallet = () => {
+        if (fs.existsSync(privateKeyLocation)) {
+            fs.unlinkSync(privateKeyLocation);
+        }
+    };
 
-  static deleteWallet = () => {
-    if(fs.existsSync(privateKeyLocation)) {
-      fs.unlinkSync(privateKeyLocation);
-    }
-  }
-
-  // static getBalance = (address: string, UTXOs: UTXO[]): number => {
-  //   return _(findUTXOs(address, UTXOs)).map((UTXO: UTXO)=>UTXO.amount).sum();
-  // }
+    // static getBalance = (address: string, UTXOs: UTXO[]): number => {
+    //   return _(findUTXOs(address, UTXOs)).map((UTXO: UTXO)=>UTXO.amount).sum();
+    // }
 }
-
-
