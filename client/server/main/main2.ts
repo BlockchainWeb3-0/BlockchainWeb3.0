b5aff5b8060d6c60c2004237acf3acaab80d7349
imysh578 <imysh578@gmail.com>
2022-01-14 16:31:01 +0900

    edit main2.ts and add npm run dev2

--- client/server/main/main2.ts
+++ client/server/main/main2.ts
@@ -1,31 +1,50 @@
-import bodyParser from "body-parser";
-import express, { Request, Response } from "express";
+import bodyParser = require("body-parser");
+import express = require("express");
+import { Request, Response } from "express";
+const helmet = require("helmet");
 import "dotenv/config";
+
 import Blockchain from "../blockchain/blockchain";
+import { getTransactionPool } from "../transactionPool/transactionPool";
+import { getPublicFromWallet, initWallet } from "../wallet/wallet";
 import { initP2PServer, getSockets, connectToPeers } from "../p2p/p2p";
+import { cors } from "./cors";
+import user = require("./routes/user");
+
+const blockchain: Blockchain = new Blockchain();
 
 const app = express();
+app.use(helmet());
+app.use(cors);
+app.disable("x-powered-by");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use("/api/user", user);
 
 export const http_port: number = 3002;
 
-////////////////////////////////////////
-// 최현석 P2P Test
 export const p2p_port: number = 6002;
-////////////////////////////////////////
 
 const initHttpServer = (port: number) => {
-    app.use(bodyParser.json());
-
     app.get("/", (req: Request, res: Response) => {
         res.send("welcome!");
     });
 
-    app.get("/blocks", (req: Request, res: Response) => {
-        const bc = new Blockchain().chain;
-        res.send(bc);
+    app.get("/balance", (req: Request, res: Response) => {
+        const balance: number = 100;
+        res.send({ balance: balance });
     });
-    ////////////////////////////////////////
-    // 최현석 P2P Test
+
+    app.get("/blocks", (req: Request, res: Response) => {
+        res.json(blockchain.chain);
+    });
+
+    app.get("/address", (req: Request, res: Response) => {
+        const address: string = getPublicFromWallet();
+        res.send({ address: address });
+    });
+
     app.get("/peers", (req, res) => {
         let sockInfo: string[] = [];
         getSockets().forEach((s: any) => {
@@ -33,8 +52,22 @@ const initHttpServer = (port: number) => {
             console.log(s._socket);
         });
         res.send(sockInfo);
-        //res.send(getSockets());
     });
+
+    app.post("/mineBlock", (req: Request, res: Response) => {
+        const data: any = req.body.data;
+        console.log("mineBlock", data);
+        blockchain.addBlock(data);
+        console.log(blockchain);
+        res.send("ok");
+    });
+
+    app.get("/transactionPool", (req, res) => {
+        console.log(getTransactionPool());
+        
+        res.send(getTransactionPool());
+    });
+
     app.post("/addPeer", (req: Request, res: Response) => {
         const data: string[] = req.body.data;
         connectToPeers(data);
@@ -43,6 +76,7 @@ const initHttpServer = (port: number) => {
     });
     ////////////////////////////////////////
 };
+initHttpServer(http_port);
 
 const server = app.listen(http_port, () => {
     console.log(`
@@ -51,11 +85,7 @@ const server = app.listen(http_port, () => {
     ####################################`);
 });
 
-initHttpServer(http_port);
-////////////////////////////////////////
-// 최현석 P2P Test
 initP2PServer(p2p_port);
 
-////////////////////////////////////////
-
+initWallet();
 export { app, server };
