078f1dc3e34dcbdf2bae8582a9d92dc00c9a35f2
mnnm <alsrhkd101@naver.com>
2022-01-19 20:22:19 +0900

    auto

--- client/src/components/mempool/MempoolPeer.jsx
+++ client/src/components/mempool/MempoolPeer.jsx
@@ -1,27 +1,22 @@
-import React from "react";
-import { Button, Spinner } from "react-bootstrap";
-import Transaction from "./Transaction";
+import React from 'react';
+import { Button, Spinner } from 'react-bootstrap';
+import Transaction from './Transaction';
 
-const MempoolPeer = ({data, handleOnClick, loading}) => {
-	return (
-		<div className="mempool-peer">
-			<div style={{ marginBottom: 20 }}>
-				<Button variant="warning" onClick={handleOnClick}>
-					Mining3001
-				</Button>
-			</div>
-			<div>
-				<h4>transaction List</h4>
-				{loading ? (
-					<Spinner animation="border" variant="dark" />
-				) : (
-					data.map((tx, index) => (
-						<Transaction key={tx.id} tx={tx} index={index} />
-					))
-				)}
-			</div>
-		</div>
-	);
+const MempoolPeer = ({ data, handleOnClick, loading, start, intervalId }) => {
+  return (
+    <div className="mempool-peer">
+      <div style={{ marginBottom: 20 }}>
+        <Button variant="warning" onClick={handleOnClick}>
+          Mining3001
+        </Button>
+        <Button onClick={start}>{intervalId ? 'Stop' : 'Start'}</Button>
+      </div>
+      <div>
+        <h4>transaction List</h4>
+        {loading ? <Spinner animation="border" variant="dark" /> : data.map((tx, index) => <Transaction key={tx.id} tx={tx} index={index} />)}
+      </div>
+    </div>
+  );
 };
 
 export default MempoolPeer;
