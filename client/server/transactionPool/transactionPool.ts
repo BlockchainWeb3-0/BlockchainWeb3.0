652fc0dded81a80b6405938e5f4d4ee42c6f23fb
Tozinoo <goldboy122@gmail.com>
2022-01-20 11:36:44 +0900

    add exceptions

--- client/server/transactionPool/transactionPool.ts
+++ client/server/transactionPool/transactionPool.ts
@@ -4,132 +4,148 @@
  * - bitcoin에서는 Mempool 이라고도 부른다.
  */
 
-import _ from "lodash";
+import _, { isNaN } from "lodash";
 import {
-	Transaction,
-	TxIn,
-	UnspentTxOut,
-	TxFunctions,
+    Transaction,
+    TxIn,
+    UnspentTxOut,
+    TxFunctions,
 } from "../transaction/transaction";
 
 class TransactionPool {
-	static getTransactionPool = (transactionPool: Transaction[]) => {
-		return _.cloneDeep(transactionPool);
-	};
+    static getTransactionPool = (transactionPool: Transaction[]) => {
+        return _.cloneDeep(transactionPool);
+    };
 
-	/**
-	 * addToTransactionPool
-	 * - transaction에 대한 검증 후 transaction pool에 추가
-	 * @param tx
-	 * @param unspentTxOuts
-	 */
-	static addToTransactionPool = (
-		tx: Transaction,
-		unspentTxOuts: UnspentTxOut[],
-		transactionPool: Transaction[]
-	) => {
-		if (!TxFunctions.validateTransaction(tx, unspentTxOuts)) {
-			throw Error("You are trying to add invalid tx to transaction pool");
-		}
+    /**
+     * addToTransactionPool
+     * - transaction에 대한 검증 후 transaction pool에 추가
+     * @param tx
+     * @param unspentTxOuts
+     */
+    static addToTransactionPool = (
+        tx: Transaction,
+        unspentTxOuts: UnspentTxOut[],
+        transactionPool: Transaction[]
+    ) => {
+        if (!TxFunctions.validateTransaction(tx, unspentTxOuts)) {
+            throw Error("You are trying to add invalid tx to transaction pool");
+        }
 
-		if (!this.isValidTxForPool(tx, transactionPool)) {
-			throw Error("You are trying to add invalid tx to transaction pool");
-		}
+        if (!this.isValidTxForPool(tx, transactionPool)) {
+            throw Error("You are trying to add invalid tx to transaction pool");
+        }
+        if (!TxFunctions.isValidTransactionStructure(tx)) {
+            throw Error("You are trying to add invalid tx to transaction pool");
+        }
+        if (
+            tx.txOuts[0].address === "" ||
+            tx.txOuts[0].address.length !== 130
+        ) {
+            throw Error("Invalid txOut address");
+        }
+        if (tx.txOuts[0].amount === 0 || isNaN(tx.txOuts[0].amount)) {
+            throw Error("Invalid txOut amount");
+        }
 
-		console.log("Successfully added to pool, tx: ", JSON.stringify(tx));
-		transactionPool.push(tx);
-	};
+        console.log("Successfully added to pool, tx: ", JSON.stringify(tx));
+        transactionPool.push(tx);
+    };
 
-	/**
-	 * hasTxIn
-	 * - txIn이 UTXO에 있는지 확인
-	 * @param txIn
-	 * @param unspentTxOuts
-	 * @returns boolean
-	 */
-	static hasTxIn = (txIn: TxIn, unspentTxOuts: UnspentTxOut[]): boolean => {
-		const foundTxIn = unspentTxOuts.find(
-			(utxo: UnspentTxOut) =>
-				utxo.txOutId === txIn.txOutId && utxo.txOutIndex === txIn.txOutIndex
-		);
-		return foundTxIn !== undefined;
-	};
+    /**
+     * hasTxIn
+     * - txIn이 UTXO에 있는지 확인
+     * @param txIn
+     * @param unspentTxOuts
+     * @returns boolean
+     */
+    static hasTxIn = (txIn: TxIn, unspentTxOuts: UnspentTxOut[]): boolean => {
+        const foundTxIn = unspentTxOuts.find(
+            (utxo: UnspentTxOut) =>
+                utxo.txOutId === txIn.txOutId &&
+                utxo.txOutIndex === txIn.txOutIndex
+        );
+        return foundTxIn !== undefined;
+    };
 
-	/**
-	 * updateTransactionPool
-	 * - 새로운 블록에 추가된 trasaction들을 transaction Pool에서 제거
-	 * @param unspentTxOuts
-	 */
-	static updateTransactionPool = (unspentTxOuts: UnspentTxOut[] | null, transactionPool: Transaction[]) => {
-		// Transaction Pool에 있는 트랜잭션 중에
-		// UTXO에 있다면 이미 새로운 블록에 포함된 것으로 취급
-		if( unspentTxOuts == null) {
-			throw Error("Invalid unspentTxOuts")
-		}
-		const invalidTxs = [];
-		for (const tx of transactionPool) {
-			for (const txIn of tx.txIns) {
-				if (!this.hasTxIn(txIn, unspentTxOuts)) {
-					invalidTxs.push(tx);
-					break;
-				}
-			}
-		}
-		// 찾은 트랜잭션들 제거
-		if (invalidTxs.length > 0) {
-			console.log(
-				"Removing the following transactions from transaction pool: ",
-				JSON.stringify(invalidTxs)
-			);
-			transactionPool = _.without(transactionPool, ...invalidTxs);
-		}
-	};
+    /**
+     * updateTransactionPool
+     * - 새로운 블록에 추가된 trasaction들을 transaction Pool에서 제거
+     * @param unspentTxOuts
+     */
+    static updateTransactionPool = (
+        unspentTxOuts: UnspentTxOut[] | null,
+        transactionPool: Transaction[]
+    ) => {
+        // Transaction Pool에 있는 트랜잭션 중에
+        // UTXO에 있다면 이미 새로운 블록에 포함된 것으로 취급
+        if (unspentTxOuts == null) {
+            throw Error("Invalid unspentTxOuts");
+        }
+        const invalidTxs = [];
+        for (const tx of transactionPool) {
+            for (const txIn of tx.txIns) {
+                if (!this.hasTxIn(txIn, unspentTxOuts)) {
+                    invalidTxs.push(tx);
+                    break;
+                }
+            }
+        }
+        // 찾은 트랜잭션들 제거
+        if (invalidTxs.length > 0) {
+            console.log(
+                "Removing the following transactions from transaction pool: ",
+                JSON.stringify(invalidTxs)
+            );
+            transactionPool = _.without(transactionPool, ...invalidTxs);
+        }
+    };
 
-	/**
-	 * getTxPoolIns
-	 * - 해당 transaction Pool에 있는 모든 Tx Input을 배열로 반환
-	 * @param aTransactionPool
-	 * @returns txIns
-	 */
-	static getTxPoolIns = (aTransactionPool: Transaction[]): TxIn[] => {
-		return _(aTransactionPool)
-			.map((tx) => tx.txIns)
-			.flatten()
-			.value();
-	};
+    /**
+     * getTxPoolIns
+     * - 해당 transaction Pool에 있는 모든 Tx Input을 배열로 반환
+     * @param aTransactionPool
+     * @returns txIns
+     */
+    static getTxPoolIns = (aTransactionPool: Transaction[]): TxIn[] => {
+        return _(aTransactionPool)
+            .map((tx) => tx.txIns)
+            .flatten()
+            .value();
+    };
 
-	/**
-	 * isValidTxForPool
-	 * - 추가될 tx의 txIns가 transactionPool에 없어야 함
-	 * @param tx
-	 * @param aTransactionPool
-	 * @returns
-	 */
-	static isValidTxForPool = (
-		tx: Transaction,
-		aTransactionPool: Transaction[]
-	): boolean => {
-		const txPoolIns: TxIn[] = this.getTxPoolIns(aTransactionPool);
+    /**
+     * isValidTxForPool
+     * - 추가될 tx의 txIns가 transactionPool에 없어야 함
+     * @param tx
+     * @param aTransactionPool
+     * @returns
+     */
+    static isValidTxForPool = (
+        tx: Transaction,
+        aTransactionPool: Transaction[]
+    ): boolean => {
+        const txPoolIns: TxIn[] = this.getTxPoolIns(aTransactionPool);
 
-		const containsTxIn = (txIns: TxIn[], txIn: TxIn) =>
-			_.find(
-				txPoolIns,
-				(txPoolIns) =>
-					txIn.txOutId === txPoolIns.txOutId &&
-					txIn.txOutIndex === txPoolIns.txOutIndex
-			);
+        const containsTxIn = (txIns: TxIn[], txIn: TxIn) =>
+            _.find(
+                txPoolIns,
+                (txPoolIns) =>
+                    txIn.txOutId === txPoolIns.txOutId &&
+                    txIn.txOutIndex === txPoolIns.txOutIndex
+            );
 
-		for (const txIn of tx.txIns) {
-			if (containsTxIn(txPoolIns, txIn)) {
-				console.log("txIn already found in the transaction pool");
-				console.log(txIn.txOutId);
-				console.log(txIn.txOutIndex);
-				
-				return false;
-			}
-		}
-		return true;
-	};
+        for (const txIn of tx.txIns) {
+            if (containsTxIn(txPoolIns, txIn)) {
+                console.log("txIn already found in the transaction pool");
+                console.log(txIn.txOutId);
+                console.log(txIn.txOutIndex);
+
+                return false;
+            }
+        }
+        return true;
+    };
 }
 
 export { TransactionPool };
--- client/src/components/p2pTransaction/P2PTransaction.jsx
+++ client/src/components/p2pTransaction/P2PTransaction.jsx
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from "react";
 import { Accordion, Button, Form, Spinner } from "react-bootstrap";
 import { ec } from "elliptic";
 import MyUTXO from "../wallet/MyUTXO";
+import { isNaN } from "lodash";
 
 const EC = new ec("secp256k1");
 
@@ -12,64 +13,88 @@ const P2PTransaction = ({ peer }) => {
     const port = peer[0];
     const initPublickey = peer[1];
     const initPrivatekey = peer[2];
-	const [publickey, setPublickey] = useState(initPublickey);
-	const [privatekey, setPrivatekey] = useState(initPrivatekey);
-	const [receiverAddress, setReceiverAddress] = useState(1);
-	const [amount, setAmount] = useState(1);
+    const [publickey, setPublickey] = useState(initPublickey);
+    const [privatekey, setPrivatekey] = useState(initPrivatekey);
+    const [receiverAddress, setReceiverAddress] = useState("");
+    const [amount, setAmount] = useState(0);
     const [peerUTXOs, setPeerUTXOs] = useState([]);
-	const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(true);
     const [balance, setBalance] = useState(0);
 
-    
     const renewWalletKeys = () => {
         const peerWalletKeys = generatePeerWallet();
-        setPublickey(peerWalletKeys[0])
-        setPrivatekey(peerWalletKeys[1])
-    }
+        setPublickey(peerWalletKeys[0]);
+        setPrivatekey(peerWalletKeys[1]);
+    };
 
-	const params = {
-		method: "post",
-		baseURL: `http://localhost:${port}`,
-		url: "/p2psendtransaction",
-		data: {
-			TxInAddress: publickey,
-			TxOutAddress: receiverAddress,
-			amount: amount,
-			sign: privatekey,
-		},
-	};
+    const params = {
+        method: "post",
+        baseURL: `http://localhost:${port}`,
+        url: "/p2psendtransaction",
+        data: {
+            TxInAddress: publickey,
+            TxOutAddress: receiverAddress,
+            amount: amount,
+            sign: privatekey,
+        },
+    };
 
-	const addTx = async () => {
-		const result = await axios.request(params);
-	};
+    const addTx = async () => {
+        if (publickey.length !== 130) {
+            alert("Invalid WalletAddress");
+            window.location.reload();
+            return false;
+        }
+        if (privatekey.length !== 64) {
+            alert("Invalid Privatekey");
+            window.location.reload();
+            return false;
+        }
+        if (receiverAddress.length !== 130) {
+            alert("Invalid Address");
+            window.location.reload();
+            return false;
+        }
+        if (isNaN(amount) || amount === 0) {
+            alert("Invalid type of amount");
+            window.location.reload();
+            return false;
+        }
+        const result = await axios.request(params);
+        console.log(result);
+        if (result.data.message === "success") {
+            alert("Transaction Success");
+            window.location.reload();
+        }
+    };
 
-	const textOnPublickey = (e) => {
-		setPublickey(e.target.value);
-	};
+    const textOnPublickey = (e) => {
+        setPublickey(e.target.value);
+    };
     const textOnPrivateKey = (e) => {
         setPrivatekey(e.target.value);
     };
-	const textOnReceiverAddress = (e) => {
-		setReceiverAddress(e.target.value);
-	};
-	const textOnAmount = (e) => {
-		setAmount(parseInt(e.target.value));
-	};
+    const textOnReceiverAddress = (e) => {
+        setReceiverAddress(e.target.value);
+    };
+    const textOnAmount = (e) => {
+        setAmount(parseInt(e.target.value));
+    };
 
     const getPeerUTXOs = async (address) => {
-		try {
-			const { data } = await axios({
-				method: "get",
-				baseURL: `http://localhost:${port}`,
-				url: `/myutxos/${publickey}`,
-			});
-			setPeerUTXOs(data);
-		} catch (error) {
-			console.error(error);
-		} finally {
-			setLoading(false);
-		}
-	};
+        try {
+            const { data } = await axios({
+                method: "get",
+                baseURL: `http://localhost:${port}`,
+                url: `/myutxos/${publickey}`,
+            });
+            setPeerUTXOs(data);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
+    };
     const getBalance = async () => {
         const result = await axios.request({
             method: "get",
@@ -79,96 +104,104 @@ const P2PTransaction = ({ peer }) => {
         setBalance(result.data.balance);
     };
 
-    useEffect(()=> {
+    useEffect(() => {
         getBalance();
         getPeerUTXOs(publickey);
-    }, [])
+    }, []);
 
-	return (
-		<div className="transaction_container">
-			<div className="transaction_title">Port {port}</div>
-			<div className="transaction_textField">
-				<TextField
-					required
-					label="Balance"
-					variant="standard"
-					name="address"
-					sx={{ width: "10%", displayPrint: "block" }}
-					value={balance}
-				/>
-				<TextField
-					required
-					label="Wallet Address"
-					variant="standard"
-					name="address"
-					onChange={textOnPublickey}
-					sx={{ width: "100%", displayPrint: "block", marginTop: "20px" }}
-					value={publickey}
-				/>
-				<TextField
-					required
-					label="PrivateKey"
-					variant="standard"
-					name="privatekey"
-					onChange={textOnPrivateKey}
-					sx={{
-						width: "100%",
-						displayPrint: "block",
-						marginTop: "20px",
-					}}
-					value={privatekey}
-				/>
-				<TextField
-					required
-					label="ReceiverAddress"
-					variant="standard"
-					name="receiverAddress"
-					onChange={textOnReceiverAddress}
-					sx={{
-						width: "100%",
-						displayPrint: "block",
-						marginTop: "20px",
-					}}
-				/>
-				<TextField
-					required
-					label="Amount"
-					variant="standard"
-					name="amount"
-					onChange={textOnAmount}
-					sx={{
-						width: 200,
-						displayPrint: "block",
-						marginTop: "20px",
-					}}
-				/>
-				<Button onClick={addTx} className="transaction_submit_btn">
-					Add Transaction
-				</Button>
-				<Button
-					onClick={renewWalletKeys}
-					variant="danger"
-					className="transaction_renew_btn"
-				>
-					New Wallet address
-				</Button>
-				<Accordion>
-					<Accordion.Item>
-						<Accordion.Header>UTXOs</Accordion.Header>
-						<Accordion.Body>
-							{loading ? (
-								<Spinner animation="border" variant="dark" />
-							) : (
-								peerUTXOs.map((utxo, index) => (
-									<MyUTXO key={index} utxo={utxo} index={index} />
-								))
-							)}
-						</Accordion.Body>
-					</Accordion.Item>
-				</Accordion>
-			</div>
-		</div>
-	);
+    return (
+        <div className="transaction_container">
+            <div className="transaction_title">Port {port}</div>
+            <div className="transaction_textField">
+                <TextField
+                    required
+                    label="Balance"
+                    variant="standard"
+                    name="address"
+                    sx={{ width: "10%", displayPrint: "block" }}
+                    value={balance}
+                />
+                <TextField
+                    required
+                    label="Wallet Address"
+                    variant="standard"
+                    name="address"
+                    onChange={textOnPublickey}
+                    sx={{
+                        width: "100%",
+                        displayPrint: "block",
+                        marginTop: "20px",
+                    }}
+                    value={publickey}
+                />
+                <TextField
+                    required
+                    label="PrivateKey"
+                    variant="standard"
+                    name="privatekey"
+                    onChange={textOnPrivateKey}
+                    sx={{
+                        width: "100%",
+                        displayPrint: "block",
+                        marginTop: "20px",
+                    }}
+                    value={privatekey}
+                />
+                <TextField
+                    required
+                    label="ReceiverAddress"
+                    variant="standard"
+                    name="receiverAddress"
+                    onChange={textOnReceiverAddress}
+                    sx={{
+                        width: "100%",
+                        displayPrint: "block",
+                        marginTop: "20px",
+                    }}
+                />
+                <TextField
+                    required
+                    label="Amount"
+                    variant="standard"
+                    name="amount"
+                    onChange={textOnAmount}
+                    sx={{
+                        width: 200,
+                        displayPrint: "block",
+                        marginTop: "20px",
+                    }}
+                />
+                <Button onClick={addTx} className="transaction_submit_btn">
+                    Add Transaction
+                </Button>
+                <Button
+                    onClick={renewWalletKeys}
+                    variant="danger"
+                    className="transaction_renew_btn"
+                >
+                    New Wallet address
+                </Button>
+                <Accordion>
+                    <Accordion.Item>
+                        <Accordion.Header>UTXOs</Accordion.Header>
+                        <Accordion.Body>
+                            {loading ? (
+                                <Spinner animation="border" variant="dark" />
+                            ) : (
+                                peerUTXOs.map((utxo, index) => (
+                                    <MyUTXO
+                                        key={index}
+                                        utxo={utxo}
+                                        index={index}
+                                    />
+                                ))
+                            )}
+                        </Accordion.Body>
+                    </Accordion.Item>
+                </Accordion>
+            </div>
+        </div>
+    );
 };
 
 const generatePeerWallet = () => {
@@ -178,6 +211,6 @@ const generatePeerWallet = () => {
     const publicKey = key.getPublic().encode("hex", false);
 
     return [publicKey, privateKey];
-}
+};
 
 export default P2PTransaction;
